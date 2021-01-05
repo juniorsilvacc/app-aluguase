3968852628a115e863bdcb764cdbb260cc7e285b
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-01-05 01:51:36 -0300

    routs e search do home

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Feather } from '@expo/vector-icons';
+import {TouchableOpacity} from 'react-native'
 
 import Home from './pages/Home';
 import Datail from './pages/Datail';
@@ -12,8 +13,45 @@ function Routes(){
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="home" component={Home} />
-                <Stack.Screen name="datail" component={Datail} />
+                <Stack.Screen 
+                name="home" 
+                component={Home}            
+                options={{
+                    title:'ALUGUE',
+                    headerTitleStyle:{
+                        fontFamily: 'Montserrat_700Bold'
+                    },
+                    headerRight: () => (
+                        <TouchableOpacity style={{marginRight: 15}}>
+                            <Feather
+                            name="shopping-bag"
+                            size={24}
+                            color="#000"
+                            />
+                        </TouchableOpacity>
+                    )
+                }}
+                />
+
+                <Stack.Screen 
+                name="datail" 
+                component={Datail} 
+                options={{
+                    title:'Detalhe',
+                    headerTitleStyle:{
+                        fontFamily: 'Montserrat_700Bold'
+                    },
+                    headerRight: () => (
+                        <TouchableOpacity style={{marginRight: 15}}>
+                            <Feather
+                            name="shopping-bag"
+                            size={24}
+                            color="#000"
+                            />
+                        </TouchableOpacity>
+                    )
+                }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
